14a7ee3979855f860b59d9338dd81148c51ba31c
siry siu <144174351+sirysiu@users.noreply.github.com>
2024-02-26 20:24:57 -0500

    added function

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,44 @@
   // Assignment Code
-  var password = document.getElementById("password");
+  var generateBtn = document.querySelector("#generate");
 
-  function generatePassword(){
-    var chars =
-    "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*().?ABCDEFGHIJKLMKNOPQRSTUVWXYZ";
-    var passwordLength = 12;
-    var password = "";
-
-    for (var i = 0; i <= passwordLength; i++){
-      var randomNumber = Math.floor(Math.random() * chars.length);
-      password += chars.substring(randomNumber, randomNumber +1);
-    }
-    document.getElementById("password").value = password;
+  function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+  
+    passwordText.value = password;
   }
+  
+  if (!hasLowercase || !hasUppercase) {
+    alert ("Choose at least one character type.");
+    return;
+  }
+  
+  if (hasLowercase ) {
+    possible += lowercase;
+  }
+  
+  if (hasUppercase) {
+    possible += uppercase;
+  }
+  
+  
+    var lengthInput=12;
+    var hasLowercase = true;
+    var hasUppercase= true;
+    var hasNumeric= false;
+    var hasSpecial= false;
+  
+    for (var i = 0; i < lengthInput; i++) {
+      var random = Math.floor(Math.random() * lowercase.length);
+      password += possible[random];
+    }
 
+    document.getElementById('password.').value
+      password;
+  
 
-
+  }
+   
   // Write password to the #password input
   function writePassword() {
     var password = generatePassword();
@@ -26,4 +49,20 @@
   }
   
   // Add event listener to generate button
-  // generateBtn.addEventListener("click", writePassword); 
+
+  const resultEl = document.getElementById('result'); 
+const lengthEl = document.getElementById('length'); 
+const uppercaseEl = document.getElementById('uppercase'); 
+const lowercaseEl = document.getElementById('lowercase'); 
+const numbersEl = document.getElementById('numbers'); 
+const symbolsEl = document.getElementById('symbols'); 
+const generateEl = document.getElementById('generate'); 
+
+
+generateBtn.addEventListener('click', () => {
+  const length = +lengthEl.value;
+  const hasLower = lowercaseEl.checked;
+  const hasUpper = uppercaseEl.checked;
+  const hasNumber = numbersEl.checked;
+  const hasSymbols = symbolsEl.checked;
+});
